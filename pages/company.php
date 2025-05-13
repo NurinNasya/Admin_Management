21ba79417230a24ef24d47ed796a43a729a3e742
AliaFiq2311 <156202633+AliaFiq2311@users.noreply.github.com>
2025-05-13 14:13:43 +0800

    updt

--- pages/company.php
+++ pages/company.php
@@ -77,8 +77,8 @@ session_start(); // Start session to access session messages
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="../pages/department.php">
-                <span class="sidenav-normal">Department</span>
+              <a class="nav-link" href="../pages/company.php">
+                <span class="sidenav-normal">Com</span>
               </a>
             </li>
             <li class="nav-item">
@@ -151,30 +151,151 @@ session_start(); // Start session to access session messages
 
 <!-- Main Content -->
  <main class="main-content position-relative border-radius-lg">
-    <div class="container-fluid py-4">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="card">
-
-            <!-- Alert Messages -->
-            <?php if (isset($_SESSION['success_message'])): ?>
-              <div class="alert alert-success alert-dismissible fade show auto-dismiss show text-white" role="alert">
-                <?= htmlspecialchars($_SESSION['success_message']) ?>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-              </div>
-              <?php unset($_SESSION['success_message']); ?>
-            <?php elseif (isset($_SESSION['error_message'])): ?>
-              <div class="alert alert-danger alert-dismissible fade show auto-dismiss show text-white" role="alert">
-                <?= htmlspecialchars($_SESSION['error_message']) ?>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-              </div>
-              <?php unset($_SESSION['error_message']); ?>
-            <?php endif; ?>
-
-            <div class="card-header d-flex justify-content-between align-items-center">
-              <h5>Company Management</h5>
-              <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addCompanyModal">Add Company</button>
+  <!-- Navbar -->
+            <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur" data-scroll="false">
+            <div class="container-fluid py-1 px-3">
+                <nav aria-label="breadcrumb">
+                <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+                    <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="javascript:;">Settings</a></li>
+                    <li class="breadcrumb-item text-sm text-white active" aria-current="page">Company</li>
+                </ol>
+                <h6 class="font-weight-bolder text-white mb-0">Company</h6>
+                </nav>
+                <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+                <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+                    <div class="input-group">
+                    <span class="input-group-text text-body"><i class="fas fa-search" aria-hidden="true"></i></span>
+                    <input type="text" class="form-control" placeholder="Type here...">
+                    </div>
+                </div>
+                <ul class="navbar-nav  justify-content-end">
+                    <li class="nav-item d-flex align-items-center">
+                    <a href="javascript:;" class="nav-link text-white font-weight-bold px-0">
+                        <i class="fa fa-user me-sm-1"></i>
+                        <span class="d-sm-inline d-none">Sign In</span>
+                    </a>
+                    </li>
+                    <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+                    <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
+                        <div class="sidenav-toggler-inner">
+                        <i class="sidenav-toggler-line bg-white"></i>
+                        <i class="sidenav-toggler-line bg-white"></i>
+                        <i class="sidenav-toggler-line bg-white"></i>
+                        </div>
+                    </a>
+                    </li>
+                    <li class="nav-item px-3 d-flex align-items-center">
+                    <a href="javascript:;" class="nav-link text-white p-0">
+                        <i class="fa fa-cog fixed-plugin-button-nav cursor-pointer"></i>
+                    </a>
+                    </li>
+                    <li class="nav-item dropdown pe-2 d-flex align-items-center">
+                    <a href="javascript:;" class="nav-link text-white p-0" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fa fa-bell cursor-pointer"></i>
+                    </a>
+                    <ul class="dropdown-menu  dropdown-menu-end  px-2 py-3 me-sm-n4" aria-labelledby="dropdownMenuButton">
+                        <li class="mb-2">
+                        <a class="dropdown-item border-radius-md" href="javascript:;">
+                            <div class="d-flex py-1">
+                            <div class="my-auto">
+                                <img src="../assets/img/team-2.jpg" class="avatar avatar-sm  me-3 ">
+                            </div>
+                            <div class="d-flex flex-column justify-content-center">
+                                <h6 class="text-sm font-weight-normal mb-1">
+                                <span class="font-weight-bold">New message</span> from Laur
+                                </h6>
+                                <p class="text-xs text-secondary mb-0">
+                                <i class="fa fa-clock me-1"></i>
+                                13 minutes ago
+                                </p>
+                            </div>
+                            </div>
+                        </a>
+                        </li>
+                        <li class="mb-2">
+                        <a class="dropdown-item border-radius-md" href="javascript:;">
+                            <div class="d-flex py-1">
+                            <div class="my-auto">
+                                <img src="../assets/img/small-logos/logo-spotify.svg" class="avatar avatar-sm bg-gradient-dark  me-3 ">
+                            </div>
+                            <div class="d-flex flex-column justify-content-center">
+                                <h6 class="text-sm font-weight-normal mb-1">
+                                <span class="font-weight-bold">New album</span> by Travis Scott
+                                </h6>
+                                <p class="text-xs text-secondary mb-0">
+                                <i class="fa fa-clock me-1"></i>
+                                1 day
+                                </p>
+                            </div>
+                            </div>
+                        </a>
+                        </li>
+                        <li>
+                        <a class="dropdown-item border-radius-md" href="javascript:;">
+                            <div class="d-flex py-1">
+                            <div class="avatar avatar-sm bg-gradient-secondary  me-3  my-auto">
+                                <svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                <title>credit-card</title>
+                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                    <g transform="translate(-2169.000000, -745.000000)" fill="#FFFFFF" fill-rule="nonzero">
+                                    <g transform="translate(1716.000000, 291.000000)">
+                                        <g transform="translate(453.000000, 454.000000)">
+                                        <path class="color-background" d="M43,10.7482083 L43,3.58333333 C43,1.60354167 41.3964583,0 39.4166667,0 L3.58333333,0 C1.60354167,0 0,1.60354167 0,3.58333333 L0,10.7482083 L43,10.7482083 Z" opacity="0.593633743"></path>
+                                        <path class="color-background" d="M0,16.125 L0,32.25 C0,34.2297917 1.60354167,35.8333333 3.58333333,35.8333333 L39.4166667,35.8333333 C41.3964583,35.8333333 43,34.2297917 43,32.25 L43,16.125 L0,16.125 Z M19.7083333,26.875 L7.16666667,26.875 L7.16666667,23.2916667 L19.7083333,23.2916667 L19.7083333,26.875 Z M35.8333333,26.875 L28.6666667,26.875 L28.6666667,23.2916667 L35.8333333,23.2916667 L35.8333333,26.875 Z"></path>
+                                        </g>
+                                    </g>
+                                    </g>
+                                </g>
+                                </svg>
+                            </div>
+                            <div class="d-flex flex-column justify-content-center">
+                                <h6 class="text-sm font-weight-normal mb-1">
+                                Payment successfully completed
+                                </h6>
+                                <p class="text-xs text-secondary mb-0">
+                                <i class="fa fa-clock me-1"></i>
+                                2 days
+                                </p>
+                            </div>
+                            </div>
+                        </a>
+                        </li>
+                    </ul>
+                    </li>
+                </ul>
+                </div>
             </div>
+            </nav>
+    <!--start main content-->        
+    <div class="container-fluid py-4">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header d-flex justify-content-between align-items-center" style="margin-bottom: 5px;">
+            <h5 style="margin-bottom: 0;">Company Management</h5>
+            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addCompanyModal" style="margin-top: 0;">Add Company</button>
+          </div>
+          <div class="card-body" style="padding-top: 10px;">
+
+      <!-- Alert Messages -->
+            <?php if (isset($_SESSION['success_message']) || isset($_SESSION['error_message'])): ?>
+            <div class="w-50">
+              <?php if (isset($_SESSION['success_message'])): ?>
+                <div class="alert alert-success alert-dismissible fade show auto-dismiss text-white" role="alert">
+                  <?= htmlspecialchars($_SESSION['success_message']) ?>
+                  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+                <?php unset($_SESSION['success_message']); ?>
+              <?php elseif (isset($_SESSION['error_message'])): ?>
+                <div class="alert alert-danger alert-dismissible fade show auto-dismiss text-white" role="alert">
+                  <?= htmlspecialchars($_SESSION['error_message']) ?>
+                  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+                <?php unset($_SESSION['error_message']); ?>
+              <?php endif; ?>
+            </div>
+          <?php endif; ?>
+
 
             <div class="card-body">
               <table class="table table-striped">
@@ -188,49 +309,61 @@ session_start(); // Start session to access session messages
                   </tr>
                 </thead>
                 <tbody>
+
                 <?php
-                  $query = "SELECT * FROM company ORDER BY id DESC";
+                  // Display company list
+                  $query = "SELECT * FROM companies ORDER BY id DESC";
                   $result = $conn->query($query);
 
                   if ($result && $result->num_rows > 0) {
-                    $counter = 1;
-                    while ($row = $result->fetch_assoc()) {
-                      $id = $row['id'];
-                      $code = htmlspecialchars($row['code']);
-                      $name = htmlspecialchars($row['name']);
-                      $status = $row['status'] ? 'Active' : 'Inactive';
-                      $raw_status = $row['status'];
+                      $counter = 1; // Initialize counter for numbering
 
+                      while ($row = $result->fetch_assoc()) {
+                          $id = $row['id']; // Get the actual ID from database
+                          $code = htmlspecialchars($row['code']);
+                          $name = htmlspecialchars($row['name']);
+                          $raw_status = $row['status']; // Store raw status for modal
+
+                          // Display badge with border color based on status
+                          if ($raw_status) {
+                              $statusBadge = '<span class="badge border border-success text-success px-3 py-2">Active</span>';
+                          } else {
+                              $statusBadge = '<span class="badge border border-danger text-danger px-3 py-2">Inactive</span>';
+                          }
+
+                          echo '
+                          <tr>
+                              <td>' . $counter++ . '</td>
+                              <td>' . $code . '</td>
+                              <td>' . $name . '</td>
+                              <td>' . $statusBadge . '</td>
+                              <td>
+                                  <a href="#" class="text-primary me-5 edit-company-btn"
+                                    data-id="' . $id . '"
+                                    data-code="' . $code . '"
+                                    data-name="' . $name . '"
+                                    data-status="' . $raw_status . '"
+                                    data-bs-toggle="modal"
+                                    data-bs-target="#editCompanyModal"
+                                    title="Edit">
+                                    <i class="bi bi-pencil-square fs-4"></i>
+                                  </a>
+                                  <a href="crudCompany.php?delete_id=' . $id . '" 
+                                    class="text-danger" 
+                                    onclick="return confirm(\'Are you sure you want to delete this company?\');"
+                                    title="Delete">
+                                    <i class="bi bi-trash-fill fs-4"></i>
+                                  </a>
+                              </td>
+                          </tr>';
+                      }
+                  } else {
                       echo '
                       <tr>
-                        <td>' . $counter++ . '</td>
-                        <td>' . $code . '</td>
-                        <td>' . $name . '</td>
-                        <td>' . $status . '</td>
-                        <td>
-                          <a href="#" class="text-primary me-5 edit-company-btn"
-                            data-id="' . $id . '"
-                            data-code="' . $code . '"
-                            data-name="' . $name . '"
-                            data-status="' . $raw_status . '"
-                            data-bs-toggle="modal"
-                            data-bs-target="#editCompanyModal"
-                            title="Edit">
-                            <i class="bi bi-pencil-square fs-4"></i>
-                          </a>
-                          <a href="crudCompany.php?delete_id=' . $id . '" 
-                            class="text-danger" 
-                            onclick="return confirm(\'Are you sure you want to delete this company?\');"
-                            title="Delete">
-                            <i class="bi bi-trash-fill fs-4"></i>
-                          </a>
-                        </td>
+                          <td colspan="5" class="text-center">No company found.</td>
                       </tr>';
-                    }
-                  } else {
-                    echo '<tr><td colspan="5" class="text-center">No data found</td></tr>';
                   }
-                ?>
+                  ?>
                 </tbody>
               </table>
             </div>
@@ -283,11 +416,11 @@ session_start(); // Start session to access session messages
           <div class="modal-body">
             <input type="hidden" name="edit_id" id="edit_id">
             <div class="mb-3">
-              <label for="edit_code" class="form-label">Company Code</label>
+              <label for="edit_code" class="form-label">Company Code <span class="text-danger">*</span></label>
               <input type="text" class="form-control" name="edit_code" id="edit_code" required>
             </div>
             <div class="mb-3">
-              <label for="edit_name" class="form-label">Company Name</label>
+              <label for="edit_name" class="form-label">Company Name <span class="text-danger">*</span></label>
               <input type="text" class="form-control" name="edit_name" id="edit_name" required>
             </div>
             <div class="mb-3">
@@ -319,15 +452,18 @@ session_start(); // Start session to access session messages
       });
     });
 
-    // Auto-dismiss alerts
-    setTimeout(() => {
-      const alert = document.querySelector('.auto-dismiss');
+    
+  </script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+  <script>
+    setTimeout(function () {
+      var alert = document.querySelector('.alert');
       if (alert) {
         alert.classList.remove('show');
         alert.classList.add('fade');
+        setTimeout(() => alert.remove(), 500);
       }
     }, 3000);
   </script>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 </body>
 </html>
